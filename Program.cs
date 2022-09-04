a9608e544bad33a27404614f515db87a79a745cf
DrTau <drttau@gmail.com>
2022-09-04 20:35:40 +0300

    Made release build

--- Program.cs
+++ Program.cs
@@ -1,14 +1,5 @@
-using Microsoft.AspNetCore.Authentication.Certificate;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.Configure<KestrelServerOptions>(options =>
-{
-    options.ConfigureHttpsDefaults(options =>
-        options.ClientCertificateMode = ClientCertificateMode.RequireCertificate);
-});
 
 // Add services to the container.
 
@@ -19,9 +10,6 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton(new RegistrationController());
 
-builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
-    .AddCertificate();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,9 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
