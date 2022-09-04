016e81120393ef7e93903d762caa81a042dac0f9
DrTau <drttau@gmail.com>
2022-09-04 18:39:51 +0300

    added kestel server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.ConfigureHttpsDefaults(options =>
+        options.ClientCertificateMode = ClientCertificateMode.RequireCertificate);
+});
 
 // Add services to the container.
 
