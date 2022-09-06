ae399631324f08db0ad995f9c3a726952d154672
DrTau <drttau@gmail.com>
2022-09-07 01:42:59 +0300

    Added Jwt authorization and skin uploading

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -1,6 +1,11 @@
+using System.Security.Claims;
+using System.Text;
 using cubichi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
 
 [ApiController]
 [Route("")]
@@ -39,18 +44,55 @@ public class RegistrationController : ControllerBase
         }
     }
 
-    [HttpPost, Route("UploadSkin")]
-    public IActionResult UploadSkin(SkinUploading request)
+    // Login method.
+    [HttpPost, Route("login")]
+    public ActionResult<string> Login(UserLoginDto user)
     {
         try
         {
-            var command = new NpgsqlCommand($"SELECT 1 FROM \"users\" WHERE username = '{request.UserName}' AND password = encode(digest('{request.Password}', 'sha256'), 'hex')", _connection);
-            if (command.ExecuteScalar() == null)
-                return BadRequest("Wrong username or password");
-
-            using (var stream = new FileStream(Path.Combine(@"C:\\Users\\drtta\\Documents\\Programming\\cubichi", "kekis" + ".png"), FileMode.Create))
+            var command = new NpgsqlCommand($"SELECT 1 FROM \"users\" WHERE username = '{user.UserName}' AND password = encode(digest('{user.Password}', 'sha256'), 'hex')", _connection);
+            if (command.ExecuteScalar() != null)
             {
-                request.File.CopyTo(stream);
+                string token = CreateToken(user);
+                return Ok(token);
+            }
+            return BadRequest("Wrong username or password");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    private string CreateToken(UserLoginDto user)
+    {
+        List<Claim> claims = new List<Claim>() {
+            new Claim(ClaimTypes.Name, user.UserName)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("CubichiPashaTimDrFed"));
+        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+        var token = new JwtSecurityToken(claims: claims,
+                                         expires: DateTime.Now.AddDays(1),
+                                         signingCredentials: cred);
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
+    }
+
+    [HttpPost, Route("UploadSkin"), Authorize]
+    public IActionResult UploadSkin(IFormFile file)
+    {
+        try
+        {
+            var username = User?.Identity?.Name;
+            if (username == null)
+                return BadRequest("User not found");
+            using (var stream = new FileStream(Path.Combine(@"/home/launcher/textures/skins", username + ".png"), FileMode.Create))
+            {
+                file.CopyTo(stream);
             }
 
             return Ok("Skin Uploaded Successfully");
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        // options.Authority = "https://localhost:5001";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("CubichiPashaTimDrFed")),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 builder.Services.AddSingleton(new RegistrationController());
 
@@ -23,6 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
