9c6f569c0d8fd131aaa6f3e6ed9a779c5b655793
DrTau <drttau@gmail.com>
2022-09-11 17:41:54 +0300

    Any origin are allowed

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => options.AddPolicy(name: "cubichCors", policy => policy.WithOrigins("http://localhost:5173")
+builder.Services.AddCors(options => options.AddPolicy(name: "cubichCors", policy => policy.AllowAnyOrigin()
                                                                                           .AllowAnyHeader()
                                                                                           .AllowAnyMethod()));
 
